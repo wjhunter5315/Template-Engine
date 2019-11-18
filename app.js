77f09913104b6f211a6e535d259ee133c018494e
wjhunter5315 <wjhunter5315@gmail.com>
2019-11-17 19:24:12 -0800

    Template Engine groundwork, starter files

---
+++ app.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+      {
+          name: 'userName',
+          message: 'What is your name?',
+      },
+      {
+          name: 'userId',
+          message: 'What is your ID number?',
+      }
+      {
+          type: 'list',
+          name: 'userRole',
+          message: 'What is your role?',
+          choices: ['Manager', 'Engineer', 'Intern'],
+
+      }
+  ])
+  .then(answers => {
+    console.info('Answer:', answers);
+  });
